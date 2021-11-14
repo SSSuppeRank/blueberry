3da9cab97a97dde64b37852b304137c9e93829cb
SSSupeRank <sultok.003@gmail.com>
2021-11-14 20:37:36 +0500

    kamila

---
+++ content/homepage.php
@@ -0,0 +1,7 @@
+<div class="row" style="height: 50vh; margin-top: 5%;">
+    <div class="col-3"></div>
+    <div class="col-6 text-center">
+        <div class="h1">Welcome to Blueberry shoppers:</div>
+        <h4>Our phone number: 87055151551</h4>
+    </div>
+</div>
